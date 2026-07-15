42d918d69be5d2f805f90c62b7adfc1dcc560eb1
Sunidhi Sharma <sunidhimudgil@gmail.com>
2026-07-15 17:02:59 +0530

    Time: 0 ms (100.00%) | Memory: 20.8 MB (76.94%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/set-matrix-zeroes.cpp
@@ -4,32 +4,50 @@ public:
         int m = matrix.size();
         int n = matrix[0].size();
 
-        int row[m];
-        int column[n];
-        
-        for(int i=0;i<m;i++){
-            row[i] = 0;
-        }
-        for(int i=0;i<n;i++){
-            column[i] = 0;
+        bool flag_row = false;
+        bool flag_column = false;
+
+        for(int j=0;j<n;j++){
+            if(matrix[0][j] == 0){
+                flag_row = true;
+                break;
+            }                 
         }
 
         for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                if(matrix[i][j] == 0){
-                        row[i] = 1;
-                        column[j] = 1;
+            if(matrix[i][0] == 0){
+                flag_column = true;
+                break;
+            }                 
+        }
+
+        for(int i=1;i<m;i++){
+            for(int j=1;j<n;j++){
+                if( matrix[i][j] == 0){
+                    matrix[i][0] = 0;
+                    matrix[0][j] = 0;
                 } 
             }
         }
 
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                if((row[i] || column[j]) == 1){
+        for(int i=1;i<m;i++){
+            for(int j=1;j<n;j++){
+                if(matrix[i][0] == 0 || matrix[0][j] == 0){
                     matrix[i][j] = 0;
                 } 
             }
         }
 
+        if(flag_row == true){
+        for(int j=0;j<n;j++){
+            matrix[0][j] = 0 ;               
+          }
+        }
+
+        if(flag_column == true){
+        for(int i=0;i<m;i++){
+            matrix[i][0] = 0 ;               
+          }
+        }
     }
 };
