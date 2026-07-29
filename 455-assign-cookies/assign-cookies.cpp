7c17fe4b1128f471811a150b2bd4604705dd11c0
Sunidhi Sharma <sunidhimudgil@gmail.com>
2026-07-30 03:18:13 +0530

    Time: 11 ms (39.61%) | Memory: 44.7 MB (79.00%) - LeetSync

--- 455-assign-cookies/assign-cookies.cpp
+++ 455-assign-cookies/assign-cookies.cpp
@@ -1,35 +1,37 @@
 class Solution {
 public:
     int findContentChildren(vector<int>& g, vector<int>& s) {
-        // int count = 0;
-        // int i = 0;
-        // int j = 0;
-        // while(i<g.size() && j<s.size()){
-        //     if(g[i] <= s[j]){
-        //         count++;
-        //         i++;
-        //         j++;
-        //     }
-        //     else{
-        //         j++;
-        //     }
-        // }
-        // return count;
         int count = 0;
-        int i = g.size() -1;
-        int j = s.size() -1;
+        int i = 0;
+        int j = 0;
         sort(g.begin(),g.end());
         sort(s.begin(),s.end());
-        while(i>=0 && j>=0){
+        while(i<g.size() && j<s.size()){
             if(g[i] <= s[j]){
                 count++;
-                i--;
-                j--;
+                i++;
+                j++;
             }
             else{
-                i--;
+                j++;
             }
         }
         return count;
+        // int count = 0;
+        // int i = g.size() -1;
+        // int j = s.size() -1;
+        // sort(g.begin(),g.end());
+        // sort(s.begin(),s.end());
+        // while(i>=0 && j>=0){
+        //     if(g[i] <= s[j]){
+        //         count++;
+        //         i--;
+        //         j--;
+        //     }
+        //     else{
+        //         i--;
+        //     }
+        // }
+        // return count;
     }
 };
